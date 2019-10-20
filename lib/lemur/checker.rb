1bee3673d466086a430822f60fbcabdc9e1b9795
linqueta <lincolnrodrs@gmail.com>
2019-10-20 12:35:23 -0300

    check only uniq envs

--- lib/lemur/checker.rb
+++ lib/lemur/checker.rb
@@ -10,7 +10,7 @@ module Lemur
       return unless key_sets.any?
 
       Dotenv.require_keys(key_sets.each_with_object([]) { |key_set, obj| obj << key_set.keys if key_set.clause }
-                                  .flatten)
+                                  .flatten.uniq)
     rescue Dotenv::MissingKeys => e
       raise Lemur::MissingKeys, e.message
     end
